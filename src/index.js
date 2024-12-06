608521aaf67f68efeeb1c6f39369bd6708a46905
Aaryan Tripathi <ayt131102@gmail.com>
2024-12-06 19:49:45 +0530

    Registration done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 const db = require('./db');
+const express = require('express');
+const bcrypt = require('bcrypt');
 
-db.query('SHOW DATABASES', (err,results) => {
-    if(err) throw err;
-    console.log('Database: ', results);
+const app = express();
+app.use(express.json());
+
+app.post('/register', async (req,res) => {
+    const { username ,password , role } = req.body;
+
+    const hashpassword = await bcrypt.hash(password,10);
+    const query = 'INSERT INTO users (username, password, role) VALUES (? ,? ,? )';
+    db.query(query,[username,hashpassword, role || 'user'], (err,result) => {
+        if(err){
+            console.error('Error: ', err);
+            return res.status(500).send('Error in user registration');
+        }
+
+        res.status(201).send('User registration successfull');
+    });
 });
+
+const PORT = 3000;
+app.listen(PORT, () =>{
+    console.log(`Server running at port ${PORT}`);
+})
